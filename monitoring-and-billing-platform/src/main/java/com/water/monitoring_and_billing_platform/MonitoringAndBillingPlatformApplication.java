b288bc2ea4402a8b6f6a1a302ce582db9562f8b2
Shriyanshi Sinha <shriyanshisinha@gmail.com>
2026-07-03 14:34:01 +0530

    Initial project setup with React and Spring Boot

---
+++ monitoring-and-billing-platform/src/main/java/com/water/monitoring_and_billing_platform/MonitoringAndBillingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.water.monitoring_and_billing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringAndBillingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringAndBillingPlatformApplication.class, args);
+	}
+
+}
---
+++ monitoring-and-billing-platform/src/test/java/com/water/monitoring_and_billing_platform/MonitoringAndBillingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water.monitoring_and_billing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringAndBillingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
