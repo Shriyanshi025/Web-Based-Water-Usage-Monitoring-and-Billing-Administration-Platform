094d00faf0abca4fbfd664c8de2f6d61b5e4574c
Shriyanshi Sinha <shriyanshisinha@gmail.com>
2026-07-14 13:03:59 +0530

    Use production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { STORAGE_KEYS } from "../constants/storageKeys";
 import { storageHelper } from "../helpers/storageHelper";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
